347399492e70f723f16f6b44ceac0e69d042e5a8
Aung <akhantz250@gmail.com>
2022-06-25 20:49:27 +0800

    gameboard features

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -3,12 +3,18 @@ import { Ship } from './ship';
 const Gameboard = function () {
   const gameboard = _createBoard();
 
+  // ship factories
   const carrier = Ship(5, 'carrier');
   const battleship = Ship(4, 'battleship');
   const destroyer = Ship(3, 'destroyer');
   const submarine = Ship(3, 'submarine');
   const boat = Ship(2, 'boat');
 
+  // getters
+  const getGameboard = () => gameboard;
+  const getShipLocation = () => shipLocation;
+
+  // ship location data
   const shipLocation = {
     carrier: [],
     battleship: [],
@@ -33,57 +39,31 @@ const Gameboard = function () {
     if (gameboard[x][y] !== 'O') return false;
     else if (gameboard[x][y] === 'O') return true;
   }
-  // stores ships' location in object
-  function _storeShipLoaction(x, y, ship) {
+  // pass x,y to ship location
+  function _storeShipLoaction(x, y, shipname) {
     const xpos = x;
     const ypos = y;
-    switch (ship) {
-      case 'carrier':
-        shipLocation.carrier.push({ x: xpos, y: ypos });
-        break;
-      case 'battleship':
-        shipLocation.battleship.push({ x: xpos, y: ypos });
-        break;
-      case 'destroyer':
-        shipLocation.destroyer.push({ x: xpos, y: ypos });
-        break;
-      case 'submarine':
-        shipLocation.submarine.push({ x: xpos, y: ypos });
-        break;
-      case 'boat':
-        shipLocation.submarine.push({ x: xpos, y: ypos });
-        break;
-      default:
-        return false;
+    shipLocation[shipname].push({ x: xpos, y: ypos });
+  }
+  function _getShipByName(name) {
+    if (name === 'carrier') {
+      return carrier;
+    } else if (name === 'battleship') {
+      return battleship;
+    } else if (name === 'destroyer') {
+      return destroyer;
+    } else if (name === 'submarine') {
+      return submarine;
+    } else if (name === 'boat') {
+      return boat;
     }
   }
-
-  const getGameboard = () => gameboard;
-  const getShipLocation = () => shipLocation;
-
   // place ship by the "head"
-  const placeShip = function (x, y, ship, horizontal) {
+  const placeShip = function (x, y, shipname, horizontal) {
     if (x > 9 || y > 9) return false;
-    let length;
-    switch (ship) {
-      case 'carrier':
-        length = 5;
-        break;
-      case 'battleship':
-        length = 4;
-        break;
-      case 'destroyer':
-        length = 3;
-        break;
-      case 'submarine':
-        length = 3;
-        break;
-      case 'boat':
-        length = 2;
-        break;
-      default:
-        return false;
-    }
+    const ship = _getShipByName(shipname);
+    const length = ship.shipLength;
+
     if (horizontal) {
       // check bounds
       if (x + length > 10) return false;
@@ -93,7 +73,7 @@ const Gameboard = function () {
       }
       for (let j = 0; j < length; j++) {
         gameboard[x + j][y] = 'X';
-        _storeShipLoaction(x + j, y, ship);
+        _storeShipLoaction(x + j, y, shipname);
       }
       return true;
     } else {
@@ -105,34 +85,47 @@ const Gameboard = function () {
       }
       for (let j = 0; j < length; j++) {
         gameboard[x][y + j] = 'X';
-        _storeShipLoaction(x, y + j, ship);
+        _storeShipLoaction(x, y + j, shipname);
       }
       return true;
     }
   };
-
+  // checks the gameboard, X is unsunk ship, O is unhit water, S is sunk ship, H is hit water
   const receiveAttack = function (x, y) {
     if (gameboard[x][y] === 'X' || gameboard[x][y] === 'O') {
       return true;
     } else return false;
   };
-
+  // checks if ship was struck and applys hit to ship instance
   const struckShip = function (x, y) {
     for (const key in shipLocation) {
+      const ship = _getShipByName(key);
       const length = shipLocation[key].length;
+      let position = 1;
       for (let i = 0; i < length; i++) {
         if (
           shipLocation[key][i]['x'] === x &&
           shipLocation[key][i]['y'] === y
         ) {
+          ship.hit(position);
           return true;
         }
+        position++;
       }
     }
     return false;
   };
-
-  const isFleetDestroyed = function () {};
+  const isFleetDestroyed = function () {
+    if (
+      carrier.isSunk() === true &&
+      battleship.isSunk() === true &&
+      destroyer.isSunk() === true &&
+      submarine.isSunk() === true &&
+      boat.isSunk() === true
+    ) {
+      return true;
+    } else return false;
+  };
 
   return {
     getGameboard,
--- src/modules/tests/gameboard.test.js
+++ src/modules/tests/gameboard.test.js
@@ -15,31 +15,31 @@ test('Places single ship correctly, case 1', () => {
 });
 
 describe('Place carrier correctly', () => {
-  test('', () => {
+  test('case 1', () => {
     const board2 = Gameboard();
     expect(board2.placeShip(9, 9, 'carrier', false)).toBe(false);
   });
-  test('', () => {
+  test('case 2', () => {
     const board2 = Gameboard();
     expect(board2.placeShip(6, 6, 'carrier', false)).toBe(false);
   });
-  test('', () => {
+  test('case 3', () => {
     const board2 = Gameboard();
     expect(board2.placeShip(5, 9, 'carrier', true)).toBe(true);
   });
-  test('', () => {
+  test('case 4', () => {
     const board2 = Gameboard();
     expect(board2.placeShip(5, 9, 'carrier', false)).toBe(false);
   });
-  test('', () => {
+  test('case 5', () => {
     const board2 = Gameboard();
     expect(board2.placeShip(9, 5, 'carrier', true)).toBe(false);
   });
-  test('', () => {
+  test('case 6', () => {
     const board2 = Gameboard();
     expect(board2.placeShip(9, 5, 'carrier', false)).toBe(true);
   });
-  test('', () => {
+  test('case 7', () => {
     const board2 = Gameboard();
     expect(board2.placeShip(10, 5, 'carrier', false)).toBe(false);
   });
@@ -75,5 +75,39 @@ describe('Attack ship', () => {
       { x: 9, y: 9 },
     ]);
     expect(board.struckShip(5, 8)).toBe(false);
+  });
+});
+
+describe('Check lose condition', () => {
+  test('lose', () => {
+    const board = Gameboard();
+    expect(board.placeShip(0, 0, 'carrier', true)).toBe(true);
+    expect(board.placeShip(0, 1, 'battleship', true)).toBe(true);
+    expect(board.placeShip(0, 2, 'destroyer', true)).toBe(true);
+    expect(board.placeShip(0, 3, 'submarine', true)).toBe(true);
+    expect(board.placeShip(0, 4, 'boat', false)).toBe(true);
+    board.struckShip(0, 0);
+    board.struckShip(1, 0);
+    board.struckShip(2, 0);
+    board.struckShip(3, 0);
+    board.struckShip(4, 0);
+
+    board.struckShip(0, 1);
+    board.struckShip(1, 1);
+    board.struckShip(2, 1);
+    board.struckShip(3, 1);
+
+    board.struckShip(0, 2);
+    board.struckShip(1, 2);
+    board.struckShip(2, 2);
+
+    board.struckShip(0, 3);
+    board.struckShip(1, 3);
+    board.struckShip(2, 3);
+
+    board.struckShip(0, 4);
+    board.struckShip(0, 5);
+
+    expect(board.isFleetDestroyed()).toBe(true);
   });
 });
